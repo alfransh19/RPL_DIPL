a31166a55d9fb0fb060e1f6b1a7384770de61d1e
Romi Rukman <romi24123r@gmail.com>
2022-06-13 22:46:27 +0700

    Create checkout.blade.php

---
+++ pustika/resources/views/checkout.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.navbar')
+@section('container')
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col md-offset-4 col-sm-offset-3">
+            <h1>Chekout</h1>
+            <h4></h4>
+        </div>
+    </div>
+@endsection
